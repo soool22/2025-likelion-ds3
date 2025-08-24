050a3de0575f668508ed37ca77f05fe95bc7bda1
b1nnnnid <suebenny2@gmail.com>
2025-08-24 20:13:55 +0900

    배포 모델 순환참조 버그: 외래키 복구

--- users/models.py
+++ users/models.py
@@ -52,9 +52,9 @@ class UserPreference(models.Model):
 
 class FavoriteStore(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='favorites')
-    #store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        #unique_together = ('user', 'store')
-        pass
+        unique_together = ('user', 'store')
+        
